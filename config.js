5f37f713d365852e408ae10e0cec08a07251b839
Ryan Makela <ryan.makela2004@gmail.com>
2026-02-22 14:21:53 -0800

    starting the project

--- config.js
+++ config.js
@@ -1,5 +1,3 @@
-// Optional: set your GitHub repo owner and name so index.html can save notes to the right repo.
-// Replace YOUR_GITHUB_USERNAME with your GitHub username. Do not put your token or any secrets here.
 var CONFIG = {
   REPO_OWNER: 'RyanTheRyan-code',
   REPO_NAME: 'SeagullNotes'
